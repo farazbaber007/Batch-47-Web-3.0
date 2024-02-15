320bddce604aa7197d3bd0f886eea975517e0d46
farazbaber007 <farazbaber007@gmail.com>
2024-02-16 01:38:07 +0500

    Updated
    lectrues step by step

---
+++ Nextjs/next/thapatechnical/yt-thapa/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+/* // https://www.youtube.com/watch?v=gV_BsB1rGBU&list=PLwGdqUZWnOp2rDbpfKAeUi9f8qZMS7_cv&index=3
+ */
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+    <h1 className="text-amber-700 font-bold"> Hello World</h1>  
+    </main>
+  );
+}
